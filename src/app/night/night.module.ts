ce40dbc46cbbbe490bfb46e00c68ece861afdbb6
FredyReyes <44854381+paisadallas@users.noreply.github.com>
2021-09-26 13:44:25 -0500

    Add
    Night clubs

--- src/app/night/night.module.ts
+++ src/app/night/night.module.ts
@@ -4,9 +4,10 @@ import { NightComponent } from './components/night/night.component';
 import { NightRoutingModule } from './night-routing.module';
 import {IvyCarouselModule} from 'angular-responsive-carousel';
 import {MaterialModule} from '../material/material.module';
+import { DaysComponent } from './components/days/days.component';
 
 @NgModule({
-  declarations: [NightComponent],
+  declarations: [NightComponent, DaysComponent],
   imports: [
     CommonModule,
     NightRoutingModule,
